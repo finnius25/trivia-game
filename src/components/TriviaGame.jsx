ff8e199adcd60f307bea0fcaae7de27aac89819b
finnius25 <finneykoshy@gmail.com>
2022-09-19 16:55:23 -0500

    style & convert markdown answers & questions

--- src/components/TriviaGame.jsx
+++ src/components/TriviaGame.jsx
@@ -1,30 +1,52 @@
 import React from "react";
+import MarkdownView from "react-showdown";
 
 function TriviaGame(props) {
   const triviaElement = props.triviaData.map((data) => {
-    const correctAnswer = { isCorrect: true, text: data.correct_answer };
+    const correctAnswer = { isCorrect: "true", text: data.correct_answer };
     const incorrectAnswers = data.incorrect_answers.map((item) => {
-      return { isCorrect: false, text: item };
+      return { isCorrect: "false", text: item };
     });
 
     const options = incorrectAnswers.concat(correctAnswer);
 
     const shuffledOptions = options.map((option) => {
-      return <button>{option.text}</button>
+      return (
+        <button className="shuffled-options">
+          <MarkdownView
+            markdown={option.text}
+            options={{ tables: true, emoji: true }}
+          />
+        </button>
+      );
     });
 
-    const questionElement = <h2>{data.question}</h2>;
+    const questionElement = (
+      <h2 className="question-element">
+        <MarkdownView
+          markdown={data.question}
+          options={{ tables: true, emoji: true }}
+        />
+      </h2>
+    );
 
     return (
       <div>
         {questionElement}
-        {shuffledOptions}
+        <div className="shuffled-options-container">{shuffledOptions}</div>
+        <hr className="solid" />
       </div>
     );
   });
 
-
-  return <div>{triviaElement}</div>;
+  return (
+    <div className="trivia-game-container">
+      {triviaElement}
+      <div>
+        <button className="answer-check-btn">Check Answers</button>
+      </div>
+    </div>
+  );
 }
 
 export default TriviaGame;
