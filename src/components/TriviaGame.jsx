94c22b4f237a008a21f7a7ddd8d0515f6402e628
finnius25 <finneykoshy@gmail.com>
2022-09-19 20:49:59 -0500

    resolved key prop error

--- src/components/TriviaGame.jsx
+++ src/components/TriviaGame.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import MarkdownView from "react-showdown";
+import { nanoid } from "nanoid";
 
 function TriviaGame(props) {
-  const triviaElement = props.triviaData.map((data) => {
-    const correctAnswer = { isCorrect: "true", text: data.correct_answer };
+  const triviaElement = props.triviaData.map((data, index) => {
+    const correctAnswer = {
+      isCorrect: "true",
+      text: data.correct_answer,
+      id: nanoid(),
+    };
     const incorrectAnswers = data.incorrect_answers.map((item) => {
-      return { isCorrect: "false", text: item };
+      return { isCorrect: "false", text: item, id: nanoid() };
     });
 
     const options = incorrectAnswers.concat(correctAnswer);
 
     const shuffledOptions = options.map((option) => {
       return (
-        <button className="shuffled-options">
+        <button key={option.id} className="shuffled-options">
           <MarkdownView
             markdown={option.text}
             options={{ tables: true, emoji: true }}
@@ -30,8 +35,9 @@ function TriviaGame(props) {
       </h2>
     );
 
+
     return (
-      <div>
+      <div key={index} >
         {questionElement}
         <div className="shuffled-options-container">{shuffledOptions}</div>
         <hr className="solid" />
